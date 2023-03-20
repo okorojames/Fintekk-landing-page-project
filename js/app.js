3ad385b84c643b50d596f551d7d99dad86c15906
okorojames <okorojameschizaram@gmail.com>
2023-03-20 22:34:10 +0100

    faq
    added and implemented the faq section toggl efunctionalities

--- js/app.js
+++ js/app.js
@@ -1,4 +1,15 @@
+// active users counter jquery code.
 $(".users--count").counterUp({
   delay: 1,
   time: 300,
+});
+//
+//
+// faqs section toggle drop down code.
+const faqs = document.querySelectorAll(".faq");
+faqs.forEach((faq) => {
+  faq.addEventListener("click", function (e) {
+    e.preventDefault();
+    faq.classList.toggle("faq--active");
+  });
 });
